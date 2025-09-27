12126d627e292c601a49ec6e98aa891ccd243282
yt6983138 <yt6983138@gmail.com>
2025-09-27 11:47:17 +0800

    Fix misimport in other projects

---
+++ GlassHeater/Utility.h
@@ -0,0 +1,10 @@
+#pragma once
+
+typedef char int8;
+typedef unsigned char uint8;
+typedef short int16;
+typedef unsigned short uint16;
+typedef long int32;
+typedef unsigned long uint32;
+typedef long long int64;
+typedef unsigned long long uint64;
