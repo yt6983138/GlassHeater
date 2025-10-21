2224f1318339f854cf045da4d0b744062f3c063a
yt6983138 <yt6983138@gmail.com>
2025-10-21 12:53:30 +0800

    Make screen work

--- GlassHeater/GlassHeater.ino
+++ GlassHeater/GlassHeater.ino
@@ -3,13 +3,15 @@
 #include "Utility.h"
 
 #define NullFunc []() {}
-#define UIUseSerial 1
+#define UIUseSerial 0
+#define DEBUG 0
 
 #if (!UIUseSerial)
 
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+//#include <Fonts/FreeSans12pt7b.h>
 
 #endif
 
@@ -19,6 +21,8 @@ enum class Status
 	ON,
 	AUTO_CLOSED
 };
+
+constexpr uint8 FORMAT_BUFFER_SIZE = 22;
 
 constexpr uint8 HEATER_MOSFET_PIN = 3;
 
@@ -36,44 +40,52 @@ NTC_Thermistor Thermistor = NTC_Thermistor(THERMISTER_PIN, R0, RN, TN, B_VALUE);
 
 double TargetTemperatureCelsius = 50.0;
 Status HeaterStatus = Status::OFF;
-uint16 AutoCloseAfterMinutes = 0;
+uint8 AutoCloseAfterMinutes = 0;
 
 #if (!UIUseSerial)
-Adafruit_SSD1306 Screen = Adafruit_SSD1306(128, 64, &Wire, 4);
+Adafruit_SSD1306 Screen = Adafruit_SSD1306(128, 64, &Wire, -1);
 #endif
 
-const String CurrentTemperatureString = "Cur.:%c%s C";
-const String CurrentStatusString = "Stat.:%c%s";
-const String TargetTemperatureString = "Target:%c%s C";
-const String TargetAutoCloseString = "Auto:%c%dmin";
 
-const String Lines[] =
+char* Lines[] =
 {
-	CurrentTemperatureString,
-	CurrentStatusString,
-	TargetTemperatureString,
-	TargetAutoCloseString
+	"Cur.:%c%s C",
+	"Stat.:%c%s",
+	"Target:%c%s C",
+	"Auto:%c%dmin"
 };
+
+char*& CurrentTemperatureString = Lines[0];
+char*& CurrentStatusString = Lines[1];
+char*& TargetTemperatureString = Lines[2];
+char*& TargetAutoCloseString = Lines[3];
+
 
 uint16 CurrentOperatingLineIndex;
 
-static inline void FormatString(String& out, const String& input, ...)
+static inline void FormatString(char*& out, char*& input, ...)
 {
-	static char buffer[64];
+	//static char buffer[32];
 
 	va_list args;
 	va_start(args, input);
 
-	memset(buffer, 0, sizeof(buffer));
-	vsnprintf(buffer, sizeof(buffer), input.c_str(), args);
-	out = String(buffer);
+#if DEBUG
+	Serial.println(input);
+#endif
+	memset(out, 0, FORMAT_BUFFER_SIZE);
+	auto count = vsnprintf(out, FORMAT_BUFFER_SIZE, input, args);
+
+#if DEBUG
+	Serial.println(count);
+#endif
 }
 static inline String FormatFloat(double value, uint8 decimalPlaces)
 {
 	return String(value, decimalPlaces);
 }
 
-static inline String StatusToString(Status stat)
+static inline char* StatusToString(Status stat)
 {
 	switch (stat)
 	{
@@ -98,12 +110,17 @@ void setup()
 
 	pinMode(THERMISTER_PIN, INPUT);
 
+#if DEBUG || UIUseSerial
 	Serial.begin(9600);
+	Serial.println("s");
+#endif
 
 #if (!UIUseSerial)
 	if (!Screen.begin(SSD1306_SWITCHCAPVCC, 0x3C))
 	{
-		Serial.println("Bad screen");
+#if DEBUG
+		Serial.println("x");
+#endif
 		while ((volatile int)1);
 	}
 #endif
@@ -126,6 +143,9 @@ void loop()
 
 static inline void UpdateHeater()
 {
+#if DEBUG
+	Serial.println("uh");
+#endif
 	auto temp = Thermistor.readCelsius();
 	if (HeaterStatus != Status::ON)
 	{
@@ -144,6 +164,9 @@ static inline void UpdateHeater()
 }
 static inline void UpdateDisplay()
 {
+#if DEBUG
+	Serial.println("udp");
+#endif
 	auto GetSelectChar = [](uint16 index) -> char
 		{
 			return (index == CurrentOperatingLineIndex) ? '>' : ' ';
@@ -151,19 +174,33 @@ static inline void UpdateDisplay()
 
 	auto currentTemperature = Thermistor.readCelsius();
 
-	String formatted[4];
+	static char* formatted[4] = 
+	{ 
+		new char[FORMAT_BUFFER_SIZE], 
+		new char[FORMAT_BUFFER_SIZE], 
+		new char[FORMAT_BUFFER_SIZE],
+		new char[FORMAT_BUFFER_SIZE]
+	};
 	FormatString(formatted[0], Lines[0], GetSelectChar(0), FormatFloat(currentTemperature, 1).c_str());
-	FormatString(formatted[1], Lines[1], GetSelectChar(1), StatusToString(HeaterStatus).c_str());
+	FormatString(formatted[1], Lines[1], GetSelectChar(1), StatusToString(HeaterStatus));
 	FormatString(formatted[2], Lines[2], GetSelectChar(2), FormatFloat(TargetTemperatureCelsius, 1).c_str());
 	FormatString(formatted[3], Lines[3], GetSelectChar(3), AutoCloseAfterMinutes);
 
 #if (!UIUseSerial)
-	for (int i = 0, cursorY = 1; i < 4; cursorY += 16, i++)
+	Screen.clearDisplay();
+	Screen.setTextWrap(false);
+	Screen.setTextSize(1);
+	Screen.setTextColor(WHITE);
+	Screen.setCursor(0, 0);
+	Screen.cp437(true);
+
+	for (int i = 0, cursorY = 0; i < 4; cursorY += 16, i++)
 	{
-		Screen.setCursor(1, cursorY);
-		Screen.setTextSize(1);
-		Screen.setTextColor(SSD1306_WHITE, SSD1306_BLACK);
 		Screen.println(formatted[i]);
+
+#if DEBUG
+		Serial.println(formatted[i]);
+#endif
 	}
 	Screen.display();
 #else
